40c2eaf9def76a5a0bfdac844732e8e520945939
denchi <den-chi@qq.com>
2013-09-02 19:20:31 +0800

    update update

--- update.cmd
+++ update.cmd
@@ -1,15 +1,33 @@
-rd web\build /s /q
-rd web\pages /s /q
-rd web\static\img\upload /s /q
+@echo off
+
+echo 正在清理过时的文件...
+if exist web\build\.git\index.lock del web\build\.git\index.lock /s/q > nul
+if exist web\pages rd web\pages /s /q > nul
+if exist web\static\img\upload rd web\static\img\upload /s /q > nul
 mkdir web\pages
-xcopy content web\pages\ /s /e
-xcopy content\image web\static\img\upload\ /s /e
-rd web\pages\image /s /q
+xcopy content web\pages\ /s /e > nul
+xcopy content\image web\static\img\upload\ /s /e > nul
+rd web\pages\image /s /q > nul
 
 set PATH=git/bin;%PATH%
-git config user.email "den-chi@qq.com"
-git config user.name "denchi"
+set PATH=../../git/bin;%PATH%
+echo 正在同步最新版本的文件...
+if exist web\build (
+    cd web\build
+    git reset --hard
+    git pull origin master
+    cd ..\..
+) else (
+    git config --global user.email "den-chi@qq.com"
+    git config --global user.name "denchi"
 git clone https://github.com/den-chi/den-chi.github.io.git web/build
+)
+@pause
+if not exist web\build (
+    echo 啊，好像更新失败了，上面写着什么呢？请截图发到user@ufoym.com吧！
+    @pause
+    exit
+)
 
 cd web\build
 del * /q
@@ -25,5 +43,6 @@ echo denchi.cn > CNAME
 set PATH=../../git/bin;%PATH%
 git add --all .
 git commit -m "update website"
+echo 如果上面显示nothing to commit，表明本次更新失败了。请截图发到user@ufoym.com吧！
 git push origin master
 @pause
